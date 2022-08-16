edf5115b896804dfb1a466c205659238356115fe
Diego Diaz moron <alu0101337975@ull.edu.es>
2022-08-16 18:33:29 +0100

    Entra en el perfil

--- AutomateBrowser.py
+++ AutomateBrowser.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 #abrir navegador
 browser = webdriver.Firefox()
 browser.implicitly_wait(5)
-browser.get('https://www.instagram.com/')
+browser.get('https://www.instagram.com')
 # aceptar cookies
 cookies_link = browser.find_element(By.XPATH, '//button[text()="Permitir solo cookies necesarias"]')
 cookies_link.click()
@@ -17,3 +17,8 @@ password_input.send_keys("Chocolate13")
 sleep(1)
 login_link = browser.find_element(By.CSS_SELECTOR,'.L3NKy > div:nth-child(1)')
 login_link.click()
+#Entrar al perfil
+icon_link = browser.find_element(By.CSS_SELECTOR, '._2dbep')
+icon_link.click()
+profile_link = browser.find_element(By.XPATH, '/html/body/div[1]/section/nav/div[2]/div/div/div[3]/div/div[6]/div[2]/div[2]/div[2]/a[1]/div/div[2]/div/div/div/div')
+profile_link.click()
