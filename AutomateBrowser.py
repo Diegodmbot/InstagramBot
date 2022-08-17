c36caa1e4c54d976dc437f43549d55873b884703
Diego Diaz moron <alu0101337975@ull.edu.es>
2022-08-17 22:28:00 +0100

    Entra en el perfil de forma silenciosa

--- AutomateBrowser.py
+++ AutomateBrowser.py
@@ -1,24 +1,37 @@
 from time import sleep
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 
 #abrir navegador
-browser = webdriver.Firefox()
+browser_options = Options()
+#browser_options.add_argument("--headless")
+browser = webdriver.Firefox(options=browser_options)
 browser.implicitly_wait(5)
 browser.get('https://www.instagram.com')
 # aceptar cookies
-cookies_link = browser.find_element(By.XPATH, '//button[text()="Permitir solo cookies necesarias"]')
-cookies_link.click()
+cookies_button = browser.find_element(By.XPATH, '/html/body/div[4]/div/div/button[2]')
+cookies_button.click()
+print("Cookies accepted")
 # ingresar usuario y contraseña
-username_input = browser.find_element(By.CSS_SELECTOR, "input[name='username']")
-password_input = browser.find_element(By.CSS_SELECTOR,"input[name='password']")
+username_input = browser.find_element(By.XPATH, '/html/body/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[1]/div/label/input')
+password_input = browser.find_element(By.XPATH,'/html/body/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[2]/div/label/input')
+#region usuario y contraseña
 username_input.send_keys("diegodm35")
 password_input.send_keys("Chocolate13")
-sleep(1)
-login_link = browser.find_element(By.CSS_SELECTOR,'.L3NKy > div:nth-child(1)')
-login_link.click()
+#endregion 
+sleep(2)
+login_button = browser.find_element(By.XPATH,'/html/body/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[3]/button/div')
+login_button.click()
+print("You are logged in")
 #Entrar al perfil
-icon_link = browser.find_element(By.CSS_SELECTOR, '._2dbep')
-icon_link.click()
-profile_link = browser.find_element(By.XPATH, '/html/body/div[1]/section/nav/div[2]/div/div/div[3]/div/div[6]/div[2]/div[2]/div[2]/a[1]/div/div[2]/div/div/div/div')
-profile_link.click()
+icon_profile = browser.find_element(By.XPATH, '/html/body/div[1]/section/nav/div[2]/div/div/div[3]/div/div[6]/div[1]/span')
+icon_profile.click()
+profile = browser.find_element(By.XPATH, '/html/body/div[1]/section/nav/div[2]/div/div/div[3]/div/div[6]/div[2]/div[2]/div[2]/a[1]/div/div[2]/div/div/div/div')
+profile.click()
+print("I am in your profile")
+browser.save_full_page_screenshot("profile.png")
+#cerrar pestaña
+sleep(10)
+print("Closing browser")
+browser.quit()
