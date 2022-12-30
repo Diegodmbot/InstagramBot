9ae87b1c043e8e3f82b714a1ec122c00cd93f872
Diegodm35 <alu0101337975@ull.edu.es>
2022-12-30 23:24:09 +0000

    test creados, algunos fallan

--- test/test_upload_photo.py
+++ test/test_upload_photo.py
@@ -1,14 +1,14 @@
-import pytest
 import sys
 from os.path import join, dirname
+import pytest
+
 
 sys.path.insert(0, join(dirname(dirname(__file__)), "src"))
 
 from InstagramBot import InstagramBot
 from Credentials import Credentials
 
-class TestFunctionalities:
-    
+class TestUploadPhoto:
     instagram_bot = None
     @pytest.fixture(autouse=True)
     def setup(self):
@@ -17,19 +17,6 @@ class TestFunctionalities:
         self.instagram_bot.run_browser()
         self.instagram_bot.accept_cookies()
         self.instagram_bot.login()
-    
-    def test_sample(self):
-        assert 1 == 1
         
-    def test_close_browser(self):
-        assert self.instagram_bot.close_browser() == True
-        
-    def test_take_screenshot(self):
-        assert self.instagram_bot.take_screenshot("test_screenshot") == True
-        
-    def test_scroll_down(self):
-        scroll_box = self.instagram_bot.browser.find_element_by_xpath('/html')
-        assert self.instagram_bot.scroll_down(scroll_box) == True
-    
-    
-    
+    def test_open_upload_photo_page(self):
+        assert self.instagram_bot.open_upload_page() == True
